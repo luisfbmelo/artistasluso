44f7d299a606784f9047af18522e70b0faa45cea
Luis Melo <geral@luisfbmelo.com>
2015-08-07 01:48:10 +0000

    Fixed facebook comments problem

--- scripts/app.js
+++ scripts/app.js
@@ -14,43 +14,42 @@ var app = angular.module('artistasluso', [
   'ngAnimate'
 ]);
 
-app.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-			when('/', {
-				templateUrl: 'scripts/views/home.html',
-				controller: 'homeCtrl'
-			}).
-			when('/project', {
-				templateUrl: 'scripts/views/project.html',
-				controller: 'projectCtrl'
-			}).
-			when('/events', {
-				templateUrl: 'scripts/views/events.html',
-				controller: 'eventsCtrl'
-			}).
-			when('/events/:id', {
-				templateUrl: 'scripts/views/event-detail.html',
-				controller: 'eventsDetailsCtrl'
-			}).
-			when('/artists', {
-				templateUrl: 'scripts/views/artists.html',
-				controller: 'artistsCtrl'
-			}).
-			when('/artists/:id', {
-				templateUrl: 'scripts/views/artists-list.html',
-				controller: 'artistsCtrl'
-			}).
-			when('/artists/details/:id', {
-				templateUrl: 'scripts/views/artist-detail.html',
-				controller: 'artistDetailsCtrl'
-			}).
-			when('/countries', {
-				templateUrl: 'scripts/views/countries.html',
-				controller: 'countriesCtrl'
-			}).
-			otherwise({
-				redirectTo: '/'
-			});
-	}
-]);
+app.config(['$routeProvider', function($routeProvider) {
+
+	$routeProvider.
+		when('/', {
+			templateUrl: 'scripts/views/home.html',
+			controller: 'homeCtrl'
+		}).
+		when('/project', {
+			templateUrl: 'scripts/views/project.html',
+			controller: 'projectCtrl'
+		}).
+		when('/events', {
+			templateUrl: 'scripts/views/events.html',
+			controller: 'eventsCtrl'
+		}).
+		when('/events/:id', {
+			templateUrl: 'scripts/views/event-detail.html',
+			controller: 'eventsDetailsCtrl'
+		}).
+		when('/artists', {
+			templateUrl: 'scripts/views/artists.html',
+			controller: 'artistsCtrl'
+		}).
+		when('/artists/:id', {
+			templateUrl: 'scripts/views/artists-list.html',
+			controller: 'artistsCtrl'
+		}).
+		when('/artists/details/:id', {
+			templateUrl: 'scripts/views/artist-detail.html',
+			controller: 'artistDetailsCtrl'
+		}).
+		when('/countries', {
+			templateUrl: 'scripts/views/countries.html',
+			controller: 'countriesCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+}]);	
--- scripts/prjscript.js
+++ scripts/prjscript.js
@@ -906,6 +906,33 @@ appDirectives.directive('facebookComments', ['$location', function ($location) {
 	    }
 	 };
 }]);
+
+app.directive('dynFbCommentBox', function () {
+    function createHTML(href, numposts, colorscheme, width) {
+        return '<div class="fb-comments" ' +
+                       'data-href="' + href + '" ' +
+                       'data-numposts="' + numposts + '" ' +
+                       'data-colorsheme="' + colorscheme + '" ' +
+                       'data-width="' + width + '">' +
+               '</div>';
+    }
+
+
+    return {
+        restrict: 'A',
+        scope: {},
+        link: function postLink(scope, elem, attrs) {
+            attrs.$observe('pageHref', function (newValue) {
+                var href        = newValue;
+                var numposts    = attrs.numposts    || 5;
+                var colorscheme = attrs.colorscheme || 'light';
+                var width = attrs.width || '100%';
+                elem.html(createHTML(href, numposts, colorscheme, width));
+                FB.XFBML.parse(elem[0]);
+            });
+        }
+    };
+});
 var appDirectives = angular.module('appDirectives');
 
 appDirectives.directive('pageFooter', [function () {
